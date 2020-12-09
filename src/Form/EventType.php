d498d8d394a760cdb3f8e8d0ecafb76136c620b9
Axel <axelcroizepro@gmail.com>
2020-12-09 10:50:19 +0100

    added crud for Event Entity

---
+++ src/Form/EventType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('theme')
+            ->add('datetime_start')
+            ->add('datetime_end')
+            ->add('place')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
