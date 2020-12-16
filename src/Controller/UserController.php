e751e488937d5afdd92d790af46707b8dd63be18
Axel <axelcroizepro@gmail.com>
2020-12-16 16:44:08 +0100

    changed routes which redirected to user index because user doesn't have to see it

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -69,7 +69,7 @@ class UserController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('user_index');
+            return $this->redirectToRoute('home_index');
         }
 
         return $this->render('user/edit.html.twig', [
@@ -89,6 +89,6 @@ class UserController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('user_index');
+        return $this->redirectToRoute('home');
     }
 }
