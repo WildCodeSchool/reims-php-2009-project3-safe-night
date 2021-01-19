e3478ec3066b0f9c2ed76b2fac75e0acc5308aa4
Axel <axelcroizepro@gmail.com>
2021-01-19 17:17:31 +0100

    feat map :retrieved user adress & locations of event they organize or get to and displayed it on the map with informations pop up

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Entity\Event;
+use App\Repository\UserRepository;
+
 
 class MapController extends AbstractController
 {
@@ -13,8 +17,19 @@ class MapController extends AbstractController
      */
     public function index(): Response
     {
+        $user = $this->getUser();
+        $eventsOrganized = $user->getEventOrganized();
+        $events = [];
+        foreach ($eventsOrganized as $eventOrganized) {
+            $events[] = $eventOrganized;
+        }
+        $eventsGoing = $user->getEventGoing();
+        foreach ($eventsGoing as $eventGoing) {
+            $events[] = $eventGoing;
+        }
         return $this->render('map/index.html.twig', [
             'controller_name' => 'MapController',
+            'events' => $events
         ]);
     }
 }
