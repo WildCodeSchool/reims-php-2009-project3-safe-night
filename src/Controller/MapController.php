c380413365e8970b4d5912abc7723a5ab032b4fb
Axel <axelcroizepro@gmail.com>
2021-01-28 16:36:12 +0100

    solved unetyped property crash in __sleep by a custom query

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -5,27 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EventRepository;
 
 class MapController extends AbstractController
 {
     /**
      * @Route("/map", name="map")
      */
-    public function index(): Response
+    public function index(EventRepository $eventRepository): Response
     {
         $user = $this->getUser();
-        $eventsOrganized = $user->getEventOrganized();
-        $events = [];
-        foreach ($eventsOrganized as $eventOrganized) {
-            $events[] = $eventOrganized;
-        }
-        $eventsGoing = $user->getEventGoing();
-        foreach ($eventsGoing as $eventGoing) {
-            $events[] = $eventGoing;
-        }
+        
         return $this->render('map/index.html.twig', [
             'controller_name' => 'MapController',
-            'events' => $events
+            'events' => $eventRepository->findAllEventRelativeToUser($user),
         ]);
     }
 }
